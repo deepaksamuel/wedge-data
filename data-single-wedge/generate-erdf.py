3f59d47a9880da8a19c0ac7e12613d4b3494906f
samuel <deepaksamuel@gmail.com>
2021-05-12 08:55:22 +0530

    --

--- data-single-wedge/generate-erdf.py
+++ data-single-wedge/generate-erdf.py
@@ -8,33 +8,44 @@ import os
 
 
 directory = "." # the folder containing all the depth-dose data
-colnames=['X', 'Y', 'Z', 'Total Dose', 'Total2','Entries']
+colnames=['x', 'y', 'z', 'd', 'd2','n']
 colnames_merged=['Energy','Position','Dose']
+e =[]
 d =[]
 
-
+df= pd.DataFrame()
+df_merged = pd.DataFrame()
 def getEnergy(fileName):
     e = fileName.split("-")
     return float(e[0])
-
+i=0
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
     if filename.endswith(".txt"): #or filename.endswith(".py"): 
-        #print(os.path.join(directory, filename))
-        print(filename)
-        #df = pd.read_csv(os.path.join(directory, filename),skiprows=3,header=0,names=colnames)   
-        df = pd.read_csv(filename,skiprows=3,header=0,names=colnames)   
-        energy =getEnergy(filename)
-        for i in range(0,299):
-            d.append([energy,df['Z'][i],df['Total Dose'][i]])
-        #plt.plot(df['Z'],df['Total Dose'])
         
-        continue
+        print(filename)
+        energy =getEnergy(filename)
+        dose_col_name = 'd-{}'.format(energy)
+        colnames=['x', 'y', 'z', dose_col_name, 'd2','n']
+        df = pd.read_csv(filename,skiprows=3,header=0,names=colnames,usecols=['x','y',dose_col_name])   
+        e.append(energy)
+        if i>0:
+            df_merged = pd.merge(df,df_merged,on=['x','y'])
+        else:
+            df_merged = df
+        i=i+1
+
     else:
         continue
-df_merged = pd.DataFrame(d,columns=colnames_merged)
-plt.plot(df_merged['Energy'],df_merged['Dose'])
-#
+
+
+# the wepl is constant along x axis and changes along y; z is the beam direction
+# select all pixels where y is constant
+newdf = df_merged.loc[(df.y == 25)]
+df = newdf.iloc[1,:]
+plt.plot(df[2:])
+plt.show()
+
 
 
 # %%
