91b29859aa17a868faaa459b84a357c4e337130a
Deepak Samuel <deepaksamuel@gmail.com>
2021-05-19 19:04:38 +0530

    --

--- scripts/generate-multi-wedge-cmds.py
+++ scripts/generate-multi-wedge-cmds.py
@@ -4,14 +4,17 @@ nthreads = 64
 min_E = 100
 max_E = 250
 step_E= 10
-nevents= 10000000
+nevents= 1000000
 
 min_tw= 0.1
-max_bw= 15
+max_bw= 20
 w_step= 0.1
 
 
 f = open("cmds", "w")
+g4path = "/DATA/software/g4-10-05-install/bin/geant4.sh"
+f.write("source {}\n".format(g4path)) 
+f.write("mkdir wedge-data-1000000-2cm\n") 
 i=0
 for tw in np.arange(min_tw,max_bw+w_step,w_step):
     for bw in np.arange(tw, max_bw+w_step, w_step):
@@ -19,6 +22,6 @@ for tw in np.arange(min_tw,max_bw+w_step,w_step):
             f.write("echo '{}) Generating for E:{} MeV top-width:{:.2g} cm bottom-width:{:.2g} cm Events:{}'\n".format(i,E,tw,bw,nevents))
             f.write("./exampleB1 {} {} {} {:.2g} {:.2g}\n".format(nthreads,E,nevents,tw,bw))
             i=i+1
-            f.write("mv *-BW.txt wedge-data-10000000\n") #this folder must preexist!!!
+            f.write("mv *-BW.txt wedge-data-1000000-2cm\n") #this folder must preexist!!!
  	
 print("Total {} files combinations. Run source cmds".format(i))
--- scripts/search-wedge.py
+++ scripts/search-wedge.py
@@ -1,4 +1,5 @@
-# search for files with the same wedge dimensions and plot the ERDF
+# search for files with the same wedge dimensions and plot the ERDF 
+# in image format using a formula (see code below)
 #%%
 import matplotlib.pyplot as plt
 import numpy as np
@@ -6,34 +7,38 @@ import pandas as pd
 import os
 
 
-data_dir = "../data-single-wedge" # the folder containing all the depth-dose data
+data_dir = "../wedge-data-10000000" # the folder containing all the depth-dose data
+dim_indx = 1 # use this to go to another dimension file
+
+
+
 colnames=['x', 'y', 'z', 'd', 'd2','n']
 colnames_merged=['Energy','Position','Dose']
 e =[]
-files =[]
-f =[]
+full_file_names =[] # full file_name
+file_name_suffix =[] # list of suffixes - to retrieve dimensions
+#240.0-MEV-1000000-EVTS-0.1-TW-0.1-BW.txt
+
 for file in os.listdir(data_dir):
     if "-EVTS-" in file:
         filename = os.fsdecode(file)
         str = filename.split("EVTS-")
-        files.append("{}/{}".format(data_dir,filename))
-        print(filename)
-        f.append(str[1])
+        full_file_names.append("{}/{}".format(data_dir,filename))
+        #print(filename)
+        file_name_suffix.append(str[1])
 
 
-print(f)
-unique_dims = np.unique(f)
+unique_dims = np.unique(file_name_suffix)
 
-# now unique_dims has the list unique wedge dimensions in the data dir
-# Now search for files having this extension 
+# now unique_dims has the list of unique wedge dimensions in the data dir
 
-wedge_files =[]
+same_dim_files =[] # files having the same dimensions
+print(unique_dims[dim_indx])
+for ff in full_file_names:
+    #print(ff)
+    if unique_dims[dim_indx] in ff:
+        same_dim_files.append(ff)
 
-for ff in files:
-    if unique_dims[0] in ff:
-        wedge_files.append(ff)
-# wedge files contain all files with the same wedge dimension
-print(wedge_files)
 def getEnergy(fileName):
     s=fileName.split("/")
     str = s[len(s)-1]
@@ -43,20 +48,20 @@ def getEnergy(fileName):
 
 i=0
 e=[]
-for file in wedge_files:
+for file in same_dim_files:
         energy =getEnergy(filename)
         print(file)
         dose_col_name = 'd-{}'.format(energy)
         colnames=['x', 'y', 'z', dose_col_name, 'd2','n']
         df = pd.read_csv("{}/{}".format(data_dir,filename),skiprows=2,header=0,names=colnames,usecols=['x','y',dose_col_name])   
-        e.append(energy)
+        e.append(energy) # store the energy of this file in a list
         if i>0:
-
-            df_merged = pd.merge(df,df_merged,on=['x','y'])
+            df_merged = pd.merge(df,df_merged,on=['x','y']) # keep x,y columns commn
         else:
             df_merged = df
         i=i+1
 
+#df_merged contains the dose measured at all the energies for the same dimensions
 n_columns=len(df_merged.columns)-2
 n_rows = len(df_merged.index)
 wepl =[]
@@ -82,4 +87,3 @@ erdf_image = np.reshape(erdf, (30, 30))
 plt.imshow(erdf_image, cmap='hot')
 plt.colorbar()
 plt.show()
-    
